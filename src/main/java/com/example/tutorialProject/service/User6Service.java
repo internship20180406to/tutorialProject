86d72992a7cae7ece2aa56a6be2b115dc8cf6f08
WID664\ffg-training <k.kikukawa@zdf.jp>
2026-08-24 13:50:34 +0900

    名前、出身地、学校の値を入れた

--- src/main/java/com/example/tutorialProject/service/User6Service.java
+++ src/main/java/com/example/tutorialProject/service/User6Service.java
@@ -7,9 +7,9 @@ import java.util.Objects;
 
 @Service
 public class User6Service {
-    String name = "";
-    String birthplace = "";
-    String school = "";
+    String name = "fujii";
+    String birthplace = "oita";
+    String school = "fit";
 
     public void user6(List<String> Users) {
         if (!Objects.equals(name, "")) {
