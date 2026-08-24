6c5bafa6c44149a0476a0232e5d7c18fa4fd7405
ffg-training <internship0216to0222@gmail.com>
2026-08-24 13:50:10 +0900

    井上　修正

--- src/main/java/com/example/tutorialProject/service/User4Service.java
+++ src/main/java/com/example/tutorialProject/service/User4Service.java
@@ -7,9 +7,9 @@ import java.util.Objects;
 
 @Service
 public class User4Service {
-    String name = " ";
-    String birthplace = " ";
-    String school = " ";
+    String name = "inoue";
+    String birthplace = "fukuoka";
+    String school = "kyudai";
 
     public void user4(List<String> Users) {
         if (!Objects.equals(name, "")) {
