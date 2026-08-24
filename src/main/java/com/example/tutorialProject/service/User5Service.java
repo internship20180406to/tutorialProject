b0771eb8734c866ef4c30bb27accd28ae224f5fb
WID665\ffg-training <uenom75@fukuoka-fg.com>
2026-08-24 13:50:48 +0900

    namura

--- src/main/java/com/example/tutorialProject/service/User5Service.java
+++ src/main/java/com/example/tutorialProject/service/User5Service.java
@@ -7,9 +7,9 @@ import java.util.Objects;
 
 @Service
 public class User5Service {
-    String name = "";
-    String birthplace ="";
-    String school = "";
+    String name = "namura";
+    String birthplace ="fukuoka";
+    String school = "fukuokajoshisyougyou";
 
     public void user5(List<String> Users) {
         if (!Objects.equals(name, "")) {
