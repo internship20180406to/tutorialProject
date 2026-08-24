79ec19e13d2a50b8107a211aed9faa3fb927b2c6
ffg-training <internship0216to0222@gmail.com>
2026-08-24 13:50:54 +0900

    倉重　修正しました。

--- src/main/java/com/example/tutorialProject/service/User3Service.java
+++ src/main/java/com/example/tutorialProject/service/User3Service.java
@@ -7,9 +7,9 @@ import java.util.Objects;
 
 @Service
 public class User3Service {
-    String name = "";
-    String birthplace = "";
-    String school = "";
+    String name = "minori kurashige";
+    String birthplace = "fukuoka";
+    String school = "fukuokajosisyougyoukoutougakkou";
 
     public void user3(List<String> Users) {
         if (!Objects.equals(name, "")) {
